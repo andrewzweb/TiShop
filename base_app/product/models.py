b3c7fc441b21462ad7de65f4dd0c93d42872420d
andrewzweb <andrewzweb@gmail.com>
2021-03-22 16:20:49 +0200

    Add model Product and test for they
    write model Product and write tests for
    make first migration

--- base_app/product/models.py
+++ base_app/product/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+''' product models '''
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+class Product(models.Model):
+    ''' model product '''
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        default=0,
+        max_digits=10,
+        decimal_places=2,
+        blank=True
+     )
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
---
+++ base_app/product/tests/test_models.py
@@ -0,0 +1,61 @@
+''' Test product models '''
+
+import decimal
+import pytest
+from django.utils.text import slugify
+from mixer.backend.django import mixer
+pytestmark = pytest.mark.django_db
+
+class TestProduct:
+    ''' test product '''
+
+    def setup(self):
+        '''set up'''
+        self.product = mixer.blend('product.Product')
+
+    def test_product_model_exist(self):
+        ''' test model exist '''
+        assert self.product.id == 1, 'Should create a Product instance with id 1'
+
+    def test_product_have_title(self):
+        ''' test product have title '''
+        product = mixer.blend('product.Product', title='product title')
+        assert product.title == 'product title'
+
+    def test_product_have_slug(self):
+        '''test product have slug'''
+        assert self.product.slug
+
+    def test_product_slug_its_title(self):
+        '''test product slug its title'''
+        assert self.product.slug == slugify(self.product.title)
+
+    def test_product_have_description(self):
+        '''test product have description'''
+        product = mixer.blend('product.Product', description='product description')
+        assert product.description == 'product description'
+
+    def test_product_have_price(self):
+        '''test product have price'''
+        product = mixer.blend('product.Product', price=10.00)
+        assert product.price == 10.00
+
+    def test_product_price_default_is_zero(self):
+        '''test product price default is zero'''
+        product = mixer.blend('product.Product')
+        assert product.price == 0
+
+    def test_product_price_max_number_max_is_ten_digits(self):
+        '''test product price max number max is ten digits'''
+        with pytest.raises(decimal.InvalidOperation):
+            mixer.blend('product.Product', price=100000000.00)
+
+    def test_product_price_have_decimal_places(self):
+        '''test product price have decimal places'''
+        product = mixer.blend('product.Product', price=1)
+        assert product.price == decimal.Decimal(1.00)
+
+    def test_product_str_return_title(self):
+        '''test product str return title'''
+        product = mixer.blend('product.Product')
+        assert product.title in str(product)
