6116249f29a19e5dca4747056e076d0c50332e44
andrewzweb <andrewzweb@gmail.com>
2021-03-23 19:18:29 +0200

    Add static settings in settings base-app

--- base_app/base_app/settings.py
+++ base_app/base_app/settings.py
@@ -117,7 +117,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+# static 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'product/static'),
+]
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
