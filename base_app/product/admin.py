351911ca4296a12f66145d029f1b442f4715f192
andrewzweb <andrewzweb@gmail.com>
2021-03-23 19:44:29 +0200

    Update admin view and add migration

--- base_app/product/admin.py
+++ base_app/product/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
+''' admin product '''
 
-# Register your models here.
+from django.contrib import admin
+from . import models
+
+
+class ProductImagesInstanceInline(admin.TabularInline):
+    model = models.ProductImage
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    ''' product admin '''
+    list_display = (
+        'view_name_and_price',
+        'title',
+        'price',
+        'updated',
+        'created')
+    list_editable = ('title', 'price')
+    search_fields = ('title', 'description')
+    list_filter = ('updated', 'created')
+
+    def view_name_and_price(self, obj):
+        ''' view name and price in one field '''
+        return "Item: {}  Price: {} usd".format(obj.title, obj.price)
+
+
+@admin.register(models.ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    ''' product image '''
+    list_display = ('description',)
